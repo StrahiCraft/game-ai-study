0070d2ba214ae15b31ded9f493e97dbf45df9e32
StrahiCraft <strahi04@gmail.com>
2026-04-26 10:19:53 +0200

    added the player

--- scripts/player/Player.cs
+++ scripts/player/Player.cs
@@ -1,15 +1,47 @@
 using Godot;
 using System;
 
-public partial class Player : Node
+public partial class Player : CharacterBody2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+	[ExportCategory("Stats")]
+	[Export] protected float _movementSpeed = 300f;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	protected Vector2 _velocity;
+
 	public override void _Process(double delta)
 	{
+		HandleInput();
+		Move();
+	}
+
+	protected virtual void HandleInput()
+	{
+		_velocity = Vector2.Zero;
+
+		if (Input.IsActionPressed("move_up"))
+		{
+			_velocity += Vector2.Up;
+		}
+		if (Input.IsActionPressed("move_down"))
+		{
+			_velocity += Vector2.Down;
+		}
+		if (Input.IsActionPressed("move_left"))
+		{
+			_velocity += Vector2.Left;
+		}
+		if (Input.IsActionPressed("move_right"))
+		{
+			_velocity += Vector2.Right;
+		}
+
+		_velocity = _velocity.Normalized() * _movementSpeed;
+	}
+
+	protected void Move()
+	{
+		UpDirection = Vector2.Up;
+		Velocity = _velocity;
+		MoveAndSlide();
 	}
 }
